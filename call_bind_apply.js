b4d9eddad824601ccd59704c4d7c37e74c752953
Akshita <ak.akshita21@gmail.com>
2023-12-19 21:11:09 -0700

    call method in js

---
+++ call_bind_apply.js
@@ -0,0 +1,27 @@
+let nameObj1 = {
+    firstName: 'Narendra',
+    lastName: 'Modi',
+}
+let printName = function(hometown, state) {
+    console.log(this.firstName + ' ' + this.lastName + ' from ' + hometown + ', ' + state);   // this keyword will point to the name object
+} 
+
+printName.call(nameObj1, 'Gandhi nagar', 'Gujrat');
+
+let nameObj2 = {
+    firstName: 'Sharukh',
+    lastName: 'Khan'
+}
+
+/*  call method: function borrowing.
+    We can borrow functions from other objects &
+    use it with the data of some other object.
+*/
+
+/*  Here, first argument will be the reference.
+    It means what we want our 'this' keyword to point to.
+
+    Later arguments will be arguments to the function.
+*/
+
+printName.call(nameObj2, ' Mumbai', 'Maharashtra');
